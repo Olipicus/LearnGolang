7dbbec3e50bb67b572baaf87c54265f3bea9fdfa
TheerapatH <thetlpc@hotmail.com>
2016-01-16 21:54:29 +0700

    Add more vaiadic experiment

--- TryVariadic/Variadic.go
+++ TryVariadic/Variadic.go
@@ -7,22 +7,15 @@ func main() {
 	fmt.Println(avg(20, 30, 28, 38))
 
 	listNum := []float64{20, 30, 28, 38}
-	fmt.Println(avgWithFloat64(listNum))
 
 	/* You Can't do this
 	fmt.Println(avg(listNum))
 	*/
 
-}
+	//But You Can do this
+	fmt.Println(avg(listNum...))
+	fmt.Println(avg([]float64{20, 30, 28, 38}...))
 
-func avgWithFloat64(listNum []float64) float64 {
-	sum := 0.0
-
-	for _, num := range listNum {
-		sum += num
-	}
-
-	return sum / float64(len(listNum))
 }
 
 func avg(listNum ...float64) float64 { //variadic function
