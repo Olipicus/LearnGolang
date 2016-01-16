301a8bc89f4cc803d63a65eeef2467c6938c84b2
TheerapatH <thetlpc@hotmail.com>
2016-01-16 16:04:30 +0700

    Re Structure Floder and add UnderStandRune

---
+++ UnderStandRune/underStandRune.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	myRune := 'A'
+	fmt.Printf("Type\tRuneVal\tStrVal\tByte\n")
+	fmt.Printf("%T\t%v\t%v\t%v\n", myRune, myRune, string(myRune), []byte(string(myRune)))
+
+	myRune = 'ก' //Thai Lang
+	fmt.Printf("Type\tRuneVal\tStrVal\tByte\n")
+	fmt.Printf("%T\t%v\t%v\t%v\n", myRune, myRune, string(myRune), []byte(string(myRune)))
+
+	myRune = 65
+	fmt.Printf("Type\tRuneVal\tStrVal\tByte\n")
+	fmt.Printf("%T\t%v\t%v\t%v\n", myRune, myRune, string(myRune), []byte(string(myRune)))
+
+}
